cc618e4e1547b6ffbf251deb09e988c83564fbd7
xhl-web <1303050061@qq.com>
2020-06-10 12:01:59 +0800

    homehader内容

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    Homehaeder
+  <div class="header">
+    <div class="header-left">
+       <span class="iconfont back-icon">&#xe743;</span>
+    </div>
+    <div class="header-text">本地生活宝</div>
+    <div class="header-right">南昌
+      <span class="iconfont bouttom-icon">&#xe6aa;</span>
+    </div>
   </div>
 </template>
 
@@ -11,6 +17,23 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="stylus" scoped>
+  .header
+    display: flex
+    line-height: .90rem
+    text-align: center
+    background-color: #6F7F96
+    color: #fff
+  .header-left
+    width: .64rem
+    float: left
+  .back-icon
+    font-size: .5rem
+  .header-text
+    flex: 1
+  .header-right
+    width: 1.24rem
+    float: right
+  .bouttom-icon
+    font-size: .3rem
 </style>
